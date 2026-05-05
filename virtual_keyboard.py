03644d1d442941861def1a2e8c464ab60f823626
Diego Espinosa <diegoespinosa2404@gmail.com>
2026-05-05 16:24:21 -0500

    fixed issue where screen only shows up then crashes

--- virtual_keyboard.py
+++ virtual_keyboard.py
@@ -4,22 +4,47 @@ import pygame
 # black background
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
-background_color = (0,0,0)
+
+r,g,b = 0,0,0
+selected = "r"
 
 running=True
+clock =pygame.time.Clock()
+
 while running: 
     for event in pygame.event.get():
-        running= False 
-    if event.type == pygame.QUIT:
-        running = False
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_1:
-            background_color = (255,0,0) #RED
-        if event.key == pygame.K_2:
-            background_color = (0,255,0)
+        if event.type == pygame.QUIT:
+            running = False
+    
+        elif event.type == pygame.KEYDOWN:
+            key = event.key 
 
-    screen.fill(background_color)
+            if key == pygame.K_r:
+                selected = "r"
+            elif key ==pygame.K_g:
+                selected = "g"
+            elif key == pygame.K_b:
+                selected = "b"
+
+            elif event.key == pygame.K_UP:
+                if selected == "r":
+                    r = min(255, r + 5)
+                elif selected == "g":
+                    g = min(255, g +5)
+                elif selected == "b":
+                    b = min(255, b +5)
+
+            elif event.key == pygame.K_DOWN:
+                if selected == "r":
+                    r = max(0, r-5)
+                elif selected == "g":
+                    g=max(0, g - 5)
+                elif selected == "b":
+                    b = max(0, b - 5)
+
+    background_color = (r,g,b)
     pygame.display.flip()
-
-    pygame.quit()
+    clock.tick(60)
+    
+pygame.quit()
 
