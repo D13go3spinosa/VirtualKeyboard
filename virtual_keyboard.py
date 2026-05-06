6a8642997a6cb9591211c98d798ddf3c763d8af4
Diego Espinosa <diegoespinosa2404@gmail.com>
2026-05-06 12:29:46 -0500

    added sound for keys asdfg,

--- virtual_keyboard.py
+++ virtual_keyboard.py
@@ -1,9 +1,12 @@
 import sounddevice as sd
+import numpy as np
 import pygame
 import random
+import threading
 
 sample_rate = 44100
 active_notes = {}
+lock = threading.Lock()
 
 key_map = {
     pygame.K_a: 261.63,
@@ -14,6 +17,30 @@ key_map = {
     pygame.K_h: 440.00,
     pygame.K_j: 493.88,
 }
+def audio_callback(outdata, frames, time, status):
+    time = np.arange(frames) / sample_rate
+    signal = np.zeros(frames)
+
+    with lock:
+        notes = list(active_notes.values())
+
+    for freq in notes:
+        signal += np.sin(2 * np.pi * freq * time)
+
+    if notes:
+        signal /= len(notes)
+
+    outdata[:] = signal.reshape(-1,1)
+
+stream = sd.OutputStream(
+        channels=1,
+        callback= audio_callback,
+        samplerate=sample_rate,
+        )
+stream.start()
+
+
+
 
 pygame.init()
 pygame.key.set_repeat(200,50)
@@ -36,11 +63,22 @@ while running:
                 random.randint(0,255)
             )
 
+            if event.key in key_map:
+                with lock:
+                    active_notes[event.key] = key_map[event.key]
+
+        elif event.type == pygame.KEYUP:
+            with lock:
+                if event.key in active_notes:
+                    del active_notes[event.key]
+        
+
               
 
     screen.fill(color)
     pygame.display.flip()
     clock.tick(60)
-    
+stream.stop()    
 pygame.quit()
+
 
